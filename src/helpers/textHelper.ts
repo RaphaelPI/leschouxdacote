830c9c50a31a05741c73d557bb885f1b3374ce2c
Antoine Rousseau <antoine@rousseau.im>
2020-12-14 16:09:30 +0100

    address links

--- src/helpers/textHelper.ts
+++ src/helpers/textHelper.ts
@@ -1,9 +1,12 @@
-export const formatAmount = (cents: number | undefined): string => {
+export const formatAmount = (cents: number | undefined) => {
   if (cents == null || typeof cents === undefined) {
     return ""
   }
   return (cents / 100).toFixed(2).replace(/(\d{1,3})\.(\d+)/g, " $1.$2 €")
 }
 
-export const getIntegerAmount = (cents: number): string => formatAmount(cents).split(".")[0]
-export const getDecimalAmount = (cents: number): string => formatAmount(cents).split(".")[1]
+export const getIntegerAmount = (cents: number) => formatAmount(cents).split(".")[0]
+export const getDecimalAmount = (cents: number) => formatAmount(cents).split(".")[1]
+
+export const getMapsLink = (producer: Producer) =>
+  `https://www.google.com/maps/search/${encodeURIComponent(producer.address)}/`
