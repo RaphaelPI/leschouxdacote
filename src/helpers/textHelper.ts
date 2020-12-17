4a4aa935f5f9634e9f1b23dad5a6c2fffc34158b
Antoine Rousseau <antoine@rousseau.im>
2020-12-18 00:16:40 +0100

    product card redesign

--- src/helpers/textHelper.ts
+++ src/helpers/textHelper.ts
@@ -1,12 +1,12 @@
-export const formatAmount = (cents: number | undefined) => {
-  if (cents == null || typeof cents === undefined) {
+export const formatAmount = (cents?: number) => {
+  if (cents == null) {
     return ""
   }
-  return (cents / 100).toFixed(2).replace(/(\d{1,3})\.(\d+)/g, " $1.$2 €")
+  return (cents / 100).toFixed(2).replace(/(\d{1,3})\.(\d+)/g, " $1,$2 €")
 }
 
-export const getIntegerAmount = (cents: number) => formatAmount(cents).split(".")[0]
-export const getDecimalAmount = (cents: number) => formatAmount(cents).split(".")[1]
+export const getIntegerAmount = (cents: number) => formatAmount(cents).split(",")[0]
+export const getDecimalAmount = (cents: number) => formatAmount(cents).split(",")[1]
 
 export const getMapsLink = (producer: Producer) =>
   `https://www.google.com/maps/search/${encodeURIComponent(producer.address)}/`
