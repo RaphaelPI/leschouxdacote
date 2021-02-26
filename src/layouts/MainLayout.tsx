27eaf4471adf6143df88628021d9cc44839498f9
Antoine Rousseau <antoine@rousseau.im>
2021-02-26 18:53:38 +0100

    footer

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -11,6 +11,11 @@ import { useUser } from "src/helpers/auth"
 import { COLORS, LAYOUT } from "src/constants"
 
 import LogoSvg from "src/assets/logo.svg"
+import Facebook from "src/assets/facebook.svg"
+import Twitter from "src/assets/twitter.svg"
+import Linkedin from "src/assets/linkedin.svg"
+
+const YEAR = new Date().getFullYear()
 
 const Header = styled.header`
   position: fixed;
@@ -51,6 +56,24 @@ const Container = styled.div<{ $bg: boolean }>`
 const Main = styled.main<{ $full?: boolean }>`
   ${({ $full }) => ($full ? `` : `max-width: ${LAYOUT.maxWidth}px; margin: 0 auto; padding: 32px;`)}
 `
+const Footer = styled.footer`
+  border-top: 1px solid ${COLORS.border};
+  padding: 24px 32px;
+  color: ${COLORS.grey};
+  font-size: 0.8em;
+  display: flex;
+  justify-content: space-between;
+  a {
+    display: inline-block;
+    svg {
+      width: 16px;
+      height: 16px;
+      vertical-align: middle;
+      margin: 0 0 0 20px;
+      fill: ${COLORS.green};
+    }
+  }
+`
 
 interface Props {
   full?: boolean
@@ -85,6 +108,25 @@ const MainLayout: FC<Props & SEOProps> = ({ full, loading, children, ...props })
       <Container $bg={isHome}>
         <Main $full={full}>{loading || wait ? <Loading /> : children}</Main>
       </Container>
+      <Footer>
+        <div>
+          © {YEAR} Les Choux d’à Côté - <a href="https://www.leschouxdacote.fr/notre-charte">Notre charte</a> -{" "}
+          <a href="https://www.leschouxdacote.fr/cgs">CGU</a> -{" "}
+          <a href="https://www.leschouxdacote.fr/mentions-l%C3%A9gales">Mentions légales</a>
+        </div>
+        <div>
+          <a href="mailto:contact@leschouxdacote.fr">Nous contacter</a>
+          <a href="https://www.facebook.com/leschouxdacote">
+            <Facebook />
+          </a>
+          <a href="https://twitter.com/leschouxdacote">
+            <Twitter />
+          </a>
+          <a href="https://www.linkedin.com/company/les-choux-d-%C3%A0-c%C3%B4t%C3%A9/">
+            <Linkedin />
+          </a>
+        </div>
+      </Footer>
     </>
   )
 }
