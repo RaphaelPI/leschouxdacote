b42d6f05b361ab5f9d9c24ec60ae7c63feeea75d
Antoine Rousseau <antoine@rousseau.im>
2021-03-19 16:49:27 +0100

    home content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,17 +8,27 @@ import LogoSvg from "src/assets/logo.svg"
 
 const Logo = styled(LogoSvg)`
   display: block;
-  margin: 9vh auto 6vh;
-  width: 75%;
-  max-width: 800px;
+  margin: 2vh auto;
+  max-width: 500px;
   @media (max-width: ${LAYOUT.mobile}px) {
     margin: 0 auto;
     width: 100%;
   }
 `
+const Title = styled.h2`
+  text-align: center;
+  @media (max-width: ${LAYOUT.mobile}px) {
+    font-size: 14px;
+    margin: 0.5em -16px;
+  }
+`
 const SearchGroup = styled(SearchBar)`
   margin: 5vh auto;
   text-align: center;
+  @media (max-width: ${LAYOUT.mobile}px) {
+    margin: 2vh auto;
+  }
+
   input {
     padding: 16px 32px;
     font-size: 1.5em;
@@ -54,12 +64,53 @@ const SearchGroup = styled(SearchBar)`
     }
   }
 `
+const Help = styled.div`
+  text-align: center;
+  ol {
+    padding-inline-start: 0;
+    list-style-position: inside;
+    li {
+      margin: 5px 0;
+    }
+  }
+  a {
+    text-decoration: underline;
+  }
+`
 
 const HomePage = () => {
   return (
     <Layout>
       <Logo />
+      <Title>
+        <strong>
+          1<sup>ère</sup> plateforme d’annonces pour l’alimentaire
+        </strong>
+        <br />
+        VENTE DIRECTE entre producteurs et acheteurs
+      </Title>
       <SearchGroup />
+      <Help>
+        <p>
+          Productrices, producteurs,
+          <br />
+          pour utiliser la plateforme, il suffit de :
+        </p>
+        <ol>
+          <li>
+            <a href="https://assistance.leschouxdacote.fr/assistance/cr%C3%A9er-son-compte">Créer son compte</a>
+          </li>
+          <li>
+            <a href="https://assistance.leschouxdacote.fr/assistance/se-connecter">Se connecter</a>
+          </li>
+          <li>
+            <a href="https://assistance.leschouxdacote.fr/assistance/publier-une-annonce">Publier des annonces</a>
+          </li>
+          <li>
+            <a href="https://assistance.leschouxdacote.fr/assistance/g%C3%A9rer-mes-annonces">Gérer ses annonces</a>
+          </li>
+        </ol>
+      </Help>
     </Layout>
   )
 }
