aec45a30b354c52179aa20bade378968a9856819
Antoine Rousseau <antoine@rousseau.im>
2021-03-19 16:49:27 +0100

    home content

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -9,16 +9,33 @@ import { Loading } from "src/components/Loader"
 import { useUser } from "src/helpers/auth"
 import { LAYOUT } from "src/constants"
 
-const Container = styled.div<{ $bg: boolean }>`
-  background-image: ${({ $bg }) => ($bg ? "url(/background.svg)" : "none")};
-  background-repeat: no-repeat;
-  background-position: bottom center;
-  background-size: contain;
+const Empty = styled.div`
   min-height: 100vh;
   padding-top: var(--header-height);
 `
-const Main = styled.main<{ $full?: boolean }>`
-  ${({ $full }) => ($full ? `` : `max-width: ${LAYOUT.maxWidth}px; margin: 0 auto; padding: 32px;`)}
+const Background = styled(Empty)`
+  background-image: url("/background.svg");
+  background-repeat: no-repeat;
+  background-position: bottom center;
+  background-size: contain;
+  @media (max-width: ${LAYOUT.mobile}px) {
+    background-size: 180%;
+  }
+  @media (orientation: portrait) and (max-height: 800px) {
+    background-size: 120%;
+  }
+  @media (orientation: portrait) and (max-height: 700px) {
+    background-image: none;
+  }
+`
+
+const MainColumn = styled.main`
+  max-width: ${LAYOUT.maxWidth}px;
+  margin: 0 auto;
+  padding: 32px;
+  @media (max-width: ${LAYOUT.mobile}px) {
+    padding: 24px;
+  }
 `
 
 interface Props {
@@ -30,12 +47,15 @@ const Layout: FC<Props & SEOProps> = ({ full, loading, children, ...props }) => 
   const { pathname } = useRouter()
   const { wait } = useUser()
 
+  const Container = pathname === "/" ? Background : Empty
+  const Main = full ? "main" : MainColumn
+
   return (
     <>
       <SEO {...props} />
       <Header />
-      <Container $bg={pathname === "/"}>
-        <Main $full={full}>{loading || wait ? <Loading /> : children}</Main>
+      <Container>
+        <Main>{loading || wait ? <Loading /> : children}</Main>
       </Container>
       <Footer />
     </>
