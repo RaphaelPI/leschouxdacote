97224843e2c4ddd6597774043ceb5db15e82eab3
Antoine Rousseau <antoine@rousseau.im>
2021-09-06 19:03:11 +0200

    follow button

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@mui/material"
+import styled from "@emotion/styled"
+
+import { useUser } from "src/helpers/auth"
+import { isFollowed } from "src/helpers/user"
+
+import IconHeartEmpty from "src/assets/icon-heart-empty.svg"
+import IconHeart from "src/assets/icon-heart.svg"
+
+const Container = styled(Button)`
+  margin: 20px auto 10px;
+  background-color: white;
+  color: #f21414;
+  text-transform: none;
+  font-weight: 300;
+  &:hover {
+    background-color: white;
+  }
+`
+
+interface Props {
+  producer: string
+}
+
+const FollowButton = ({ producer }: Props) => {
+  const { user, toggleFollow } = useUser()
+  const followed = isFollowed(producer, user)
+
+  return (
+    <Container
+      onClick={() => toggleFollow(producer, !followed)}
+      variant="contained"
+      startIcon={followed ? <IconHeart /> : <IconHeartEmpty />}
+    >
+      {followed ? "Ne plus suivre le producteur" : "Suivre le producteur"}
+    </Container>
+  )
+}
+
+export default FollowButton
